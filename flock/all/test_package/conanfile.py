857c484d16bc91e2ce4bc389e458a63517b23a8d
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-01-25 12:56:32 +0100

    flock, gamecube-tools, general-tools: refactoring test_packages

--- flock/all/test_package/conanfile.py
+++ flock/all/test_package/conanfile.py
@@ -1,21 +1,16 @@
 from conan import ConanFile
-from conan.tools.build import can_run
 from conan.tools.layout import basic_layout
 
 
 class TestPackgeConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "VirtualBuildEnv"
     test_type = "explicit"
-
-    generators = "CMakeToolchain", "CMakeDeps"
 
     def layout(self):
         basic_layout(self)
 
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def test(self):
-        if can_run(self):
-            self.run("flock --version")
+        self.run("flock --version")
--- gamecube-tools/all/test_package/conanfile.py
+++ gamecube-tools/all/test_package/conanfile.py
@@ -1,33 +1,19 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
-from conan.tools.build import can_run
 
 
 class TestPackgeConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "VirtualBuildEnv"
     test_type = "explicit"
 
-    generators = "CMakeToolchain", "CMakeDeps"
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def layout(self):
-        cmake_layout(self)
-
-    def build(self):
-        valid_os = ["NintendtoWii", "NintendoGameCube"]
-        if self.settings.get_safe("os") in valid_os:
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
+        basic_layout(self)
 
     def test(self):
-        if can_run(self):
-            self.run("gcdsptool -h")
-            self.run("elf2dol -h")
-            self.run("gxtexconv -h")
+        self.run("gcdsptool -h")
+        self.run("elf2dol -h", ignore_errors=True)
+        self.run("gxtexconv -h")
 
